f0458949681f545f44967c73738a54ecfb0f163c
dotJManuel <dotJManuel@outlook.com>
2024-07-24 20:58:36 -0600

    text message box file implementation

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, TypingLoaderComponent } from '@components/index';
+import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, TextMessageBoxFileComponent, TextMessageEvent, TypingLoaderComponent } from '@components/index';
 
 @Component({
   selector: 'app-orthography-page',
@@ -11,6 +11,7 @@ import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, Typi
     MyMessageComponent,
     TypingLoaderComponent,
     TextMessageBoxComponent,
+    TextMessageBoxFileComponent,
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,6 +20,10 @@ export default class OrthographyPageComponent {
 
     handleMessage( prompt: string ) {
 
+    }
+
+    handleMessageWithFile({ prompt, file }: TextMessageEvent) {
+        console.log({prompt, file});
     }
 
 }
